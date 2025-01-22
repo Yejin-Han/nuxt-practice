de9fe8906564f40f17938459a580a8bae867ecf4
Yejin Han (한예진) <119913434+Yejin-Han@users.noreply.github.com>
2025-01-22 13:01:06 +0900

    001-default-layout

---
+++ travel-budget-tracker/app/layouts/default.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container mx-auto max-w-xl h-full">
+    <header>header</header>
+    <main class="h-full pt-4 flex-1">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script setup>
+useHead({
+  link: [
+    {
+      rel: "preconnect",
+      href: "https://fonts.googleapis.com",
+    },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Inter&display=swap",
+      crossorigin: "",
+    },
+  ],
+  htmlAttrs: {
+    class: 'h-full',
+  },
+  bodyAttrs: {
+    class: 'h-full',
+  },
+})
+</script>
+
+<style>
+#__nuxt {
+  @apply h-full;
+}
+</style>
